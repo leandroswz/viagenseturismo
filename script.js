4a6840f796216ea553b9d5ca3dd3784aca34fada
Leandro Costa <leandrin.costta@gmail.com>
2022-03-04 22:15:56 -0300

    webcommit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+function contato (){
+    let modal = document.querySelector('.modal')
+    modal.style.display = 'block';
+}
+function fechar (){
+    let modal = document.querySelector('.modal')
+    modal.style.display = 'none';
+}
+
+// ------------------------------------------------------------
+
+function servicos (){
+    let modal2 = document.querySelector('.modal2')
+    modal2.style.display = 'block';
+}
+function fechar2 (){
+    let modal2 = document.querySelector('.modal2')
+    modal2.style.display = 'none';
+}
+
+// ------------------------------------------------------------
+
+function sobre (){
+    let modal3 = document.querySelector('.modal3')
+    modal3.style.display = 'block';
+}
+function fechar3 (){
+    let modal3 = document.querySelector('.modal3')
+    modal3.style.display = 'none';
+}
+
+
